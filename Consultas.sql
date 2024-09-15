03ba810f6d347cf3da61de1d704c67acec7e85d5
Yael Parra <74014315+Yael-Parra@users.noreply.github.com>
2024-09-15 16:39:32 +0200

    Add files via upload

---
+++ Consultas.sql
@@ -0,0 +1,45 @@
+-- Seleccionar la BBDD
+USE BBDD_CinemExtract;
+
+
+
+-- ¿Qué géneros han recibido más premios Óscar?
+habrá que ligar las tablas 
+
+-- ¿Qué género es el mejor valorado en IMDB?
+SELECT datos_peliculas.genero_pelicula AS Género
+FROM datos_peliculas
+JOIN peliculas_imdb ON datos_peliculas.id_pelicula = peliculas_imdb.id_imdb
+GROUP BY datos_peliculas.genero_pelicula
+ORDER BY MAX(peliculas_imdb.puntuacion) DESC
+LIMIT 1;
+
+-- ¿En qué año se estrenaron más películas?
+
+SELECT anio_pelicula AS "Año de estreno", COUNT(titulo_pelicula) AS "Cantidad de películas"
+FROM datos_peliculas
+GROUP BY anio_pelicula
+ORDER BY COUNT(anio_pelicula) DESC;
+
+-- ¿En qué año se estrenaron más cortos?
+SELECT  DISTINCT anio_pelicula, COUNT(id_pelicula)
+FROM datos_peliculas
+WHERE tipo_pelicula = "Short"
+GROUP BY anio_pelicula
+ORDER BY COUNT(id_pelicula) DESC 
+LIMIT 1;
+
+-- ¿Cuál es la mejor serie valorada en IMDB?
+Hay que extraer las series copiando codigo de extraccion de pelis imdb pero series imdb
+
+-- ¿Cuál es la película mejor valorada en IMDB?
+SELECT titulo_imdb AS Título, MAX(puntuacion) AS Puntuación
+FROM peliculas_imdb
+GROUP BY titulo_imdb
+ORDER BY MAX(puntuacion) DESC;
+
+-- ¿Qué actor/actriz ha recibido más premios?
+sale del codigo de actores con los premios de los actores
+
+-- ¿Hay algún actor/actriz que haya recibido más de un premio Óscar?
+hay que volver a subir los datos con el ajuste de las variables de tuplas, para no trabajar doble, nos esperamos a tener la tabla ok
