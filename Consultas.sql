39d69b487b53ef2a22f350c87b37fcf6f057b266
Yael Parra <74014315+Yael-Parra@users.noreply.github.com>
2024-09-17 09:31:07 +0200

    Add files via upload

--- Consultas.sql
+++ Consultas.sql
@@ -2,10 +2,6 @@
 USE BBDD_CinemExtract;
 
 
-
--- ¿Qué géneros han recibido más premios Óscar?
-habrá que ligar las tablas 
-
 -- ¿Qué género es el mejor valorado en IMDB?
 SELECT datos_peliculas.genero_pelicula AS Género
 FROM datos_peliculas
@@ -15,11 +11,11 @@ ORDER BY MAX(peliculas_imdb.puntuacion) DESC
 LIMIT 1;
 
 -- ¿En qué año se estrenaron más películas?
-
 SELECT anio_pelicula AS "Año de estreno", COUNT(titulo_pelicula) AS "Cantidad de películas"
 FROM datos_peliculas
 GROUP BY anio_pelicula
-ORDER BY COUNT(anio_pelicula) DESC;
+ORDER BY COUNT(anio_pelicula) DESC
+LIMIT 1;
 
 -- ¿En qué año se estrenaron más cortos?
 SELECT  DISTINCT anio_pelicula, COUNT(id_pelicula)
@@ -56,7 +52,6 @@ HAVING COUNT(*) >1;
 -- Consultas extras
 
 -- Obtener informacion sobre las ceremonias de los premios Oscar en las que una pelicula ganó en varias categorias y ordenados del más reciente al menos reciente
-
 SELECT ceremonia, mejor_pelicula, mejor_director, mejor_actor, mejor_actriz
 FROM premios_oscar
 WHERE mejor_pelicula IS NOT NULL
@@ -67,7 +62,6 @@ ORDER BY ceremonia DESC;
 
 
 -- Promedio de puntuación de los directores con por lo menos 3 peliculas dirigidas. (series)
-
 WITH directores_puntuacion AS (
     SELECT direccion, ROUND(AVG(puntuacion),2) AS PromedioPuntuacion, COUNT(*) AS NumPeliculas
     FROM peliculas_imdb
@@ -80,7 +74,6 @@ WHERE NumPeliculas >= 3
 ORDER BY PromedioPuntuacion;
 
 -- ¿Hay algún actor/actriz que haya recibido más de un premio Óscar?
-
 SELECT mejor_actor AS Actor, COUNT(*) AS "Oscares"
 FROM premios_oscar
 GROUP BY mejor_actor, mejor_actriz
